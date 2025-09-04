8c27cdaad3fcf16ea9cf2f4218b8e9bf1c25ae6d
Yasin Daniyal <yasindaniyal.frontend@gmail.com>
2025-09-04 20:24:13 +0500

    Config updated for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/yasindaniyal/", 
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
